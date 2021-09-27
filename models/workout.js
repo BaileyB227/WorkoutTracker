89e93a73d3b8916e431371177895c18c936cd889
BaileyB227 <87950291+BaileyB227@users.noreply.github.com>
2021-09-27 11:59:07 -0500

    added api.js

--- models/workout.js
+++ models/workout.js
@@ -38,6 +38,6 @@ const WorkoutSchema = new Schema({
     ]
 });
   
-  const Workout = mongoose.model("workout", WorkoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
   
-  module.exports = Workout;
+module.exports = Workout;
